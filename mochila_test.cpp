7012af003773dd5486f4eb0f8b06da130036d390
CF <something>
2023-09-28 23:24:28 +0000

    test ready

--- mochila_test.cpp
+++ mochila_test.cpp
@@ -5,33 +5,33 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
-vector<int> profit;
-int memo[100][100];
+int problemData[20][2];
 
 int backpack(int index, int bpSize) { 
+//    cout << "index: " << index << " bpSize: " << bpSize << endl;
     if (bpSize == 0) return 0;
-    if (memo[index][bpSize] != -1) return memo[index][bpSize];
+    if (index == (sizeof problemData / sizeof problemData[0] )) return 0;
+    if (problemData[index][0] > bpSize) return backpack(index + 1, bpSize);
     int r1 = backpack(index + 1, bpSize);
-    int r2 = profit[index] + backpack(index + 1, bpSize - 1);  
-    return memo[index][bpSize] = max(r1, r2);
+    int r2 = backpack(index + 1, bpSize - problemData[index][0]) + problemData[index][1];
+    //return max(r1, r2);
+    return max(r2, r1);
 }
 
 
 int main() {
-
-    for (int i = 0; i < 100; ++i) {
-        for (int j = 0; j < 100; ++j) {
-            memo[i][j] = -1;
-        }
-    }
-
-    vector<vector<int>> data;
+    
     int backpackSize = 100;
-    //format data filedData[1] = height, fileData[2] = value
-    data = {{1, 1}, {2, 6}, {5, 18}, {6, 22}, {7, 28}};
+	int profit[] = { 60, 100, 120, 10, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 10, 30, 40, 50, 60, 70, 80};
+	int weight[] = { 10, 20, 30 , 40, 50, 60, 70, 80, 90, 100, 110, 120, 10, 30, 40, 50, 60, 70, 80, 90, 100};
+    for (int i = 0; i < 20; ++i) {
+        problemData[i][0] = weight[i];
+        problemData[i][1] = profit[i];
+    }
 
     int result =  backpack(0, backpackSize);
     cout << result << endl;
